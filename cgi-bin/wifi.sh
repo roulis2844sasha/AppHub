840403ac68e714ce994ee663446f70436ff1f6cc
Alexander Roulisov <roulis2844sasha@gmail.com>
2015-11-09 19:21:55 +0600

    fix wifi

--- cgi-bin/wifi.sh
+++ cgi-bin/wifi.sh
@@ -1,26 +1,36 @@
-sudo gedit /etc/dnsmasq.conf :
+if [ $1 == 'start' ]; then
 
+
+echo 'Интерфейс wifi'
+read wifi
+echo 'Интерфейс с интернетом'
+read lan
+echo 'Название точки'
+read ssid
+echo 'Пароль точки'
+read pass
 echo "bind-interfaces
-interface=wlan0
+interface=$wifi
 dhcp-range=192.168.10.2,192.168.10.5" > /etc/dnsmasq.conf
-echo "interface=wlan0
+echo "interface=$wifi
 driver=nl80211
-ssid=Linux
+ssid=$ssid
 hw_mode=g
 channel=6
 wpa=2
-wpa_passphrase=12345678" > /etc/hostapd.conf
+wpa_passphrase=$pass" > /etc/hostapd.conf
 echo "#dns=dnsmasq" >> /etc/NetworkManager/NetworkManager.conf
 restart network-manager
-if [ $1 == 'start' ]; then
-    ifconfig wlan0 192.168.10.1
+
+
+    ifconfig $wifi 192.168.10.1
     service dnsmasq restart
     sysctl net.ipv4.ip_forward=1
-    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  #Вместо eth0 нужно прописать твой интерфейс через который подключается Интернет
+    iptables -t nat -A POSTROUTING -o $lan -j MASQUERADE
     hostapd /etc/hostapd.conf
 fi
 if [ $1 == 'stop' ]; then
-    iptables -D POSTROUTING -t nat -o eth0 -j MASQUERADE    #Вместо eth0 нужно прописать твой интерфейс через который подключается Интернет
+    iptables -D POSTROUTING -t nat -o $lan -j MASQUERADE
     sysctl net.ipv4.ip_forward=0
     service dnsmasq stop
     service hostapd stop
